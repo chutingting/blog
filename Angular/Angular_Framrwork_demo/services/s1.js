188edc57be1db88013c81332f8693e1c83de09f1
Stringbao <String_bao@163.com>
2016-05-31 16:23:46 +0800

    edit

--- Angular/Angular_Framrwork_demo/services/s1.js
+++ Angular/Angular_Framrwork_demo/services/s1.js
@@ -2,7 +2,7 @@
  * Created by wupeng5 on 2016/5/9.
  */
 
-angular.module('app').factory("s1",function($http){
+angular.module('app').service("s1",function($http){
     var t = function(){
         this.name = 1;
     }
