ea2e403b7b97753028f1737c918d9d2d9f983128
Stringbao <String_bao@163.com>
2016-06-03 18:58:59 +0800

    edit js core.js

--- Js_Core_Methods/core.js
+++ Js_Core_Methods/core.js
@@ -39,14 +39,32 @@
         return re;
     }
 
-    //删除数组的某项,只针对数组里面为引用类型
+    //删除数组的某项, 值类型和引用类型均可
     Array.prototype.remove = function(item){
-        var index = this.indexOf(item);
-        if(index >= 0){
-            this.splice(index,1);
-            return true;
+
+        if(!item){
+            return false;
         }
-        return false;
+
+        if(typeof item != "object"){
+            var index = this.indexOf(item);
+            if(index >= 0){
+                this.splice(index,1);
+                return true;
+            }
+            return false;
+        }else{
+            var _index = -1;
+            for(var i=0;i<this.length;i++){
+                if(item.equals(this[i]) == true){
+                    _index = i;
+                    break;
+                }
+            }
+
+            return this.removeAt(_index);
+        }
+
     }
 
     //根据索引删除数组的某项
@@ -58,40 +76,60 @@
         return false;
     }
 
-    //检测数组是否包含key为value的项
+    //检测数组是否包含key为value的项,只针对引用类型
     Array.prototype.containsByKey = function(key,value){
         if (key && typeof key == 'string' && (value || value == 0)) {
+            var bl = false;
             for (var i = 0; i < this.length; i++) {
                 var currentObj = this[i];
-
-                if(typeof currentObj[key] == "string"){
-                    if (currentObj[key] == value) {
-                        return true;
-                    }
+                if (currentObj[key].equals(value) == true) {
+                    bl = true;
+                    break;
                 }
-                if(typeof currentObj[key] == "object"){
-                    if (currentObj[key].equals(value) == true) {
-                        return true;
-                    }
+            }
+            return bl;
+        }
+        return false;
+    }
+
+    //检测数组是否包含某一个数据类型或者对象类型
+    Array.prototype.contains = function(item){
+        if(!item){
+            return false;
+        }
+
+        var bl = false;
+        if (typeof item != 'object') {
+            this.indexOf(item) == -1 ? bl = false: bl= true;
+        }
+        else{
+            var bl = false;
+            for(var i=0;i<this.length;i++){
+                if(this[i].equals(item)){
+                    bl = true;
+                    break;
                 }
-
-
             }
         }
-        return false;
+        return bl;
     }
 
-    //检测数组是否包含某一个对象
-    Array.prototype.contains = function(item){
-        if (typeof item != 'undefined') {
-            return this.find(item, true) != undefined;
+    //数组的复制,值类型和引用类型均可
+    Array.prototype.cloneArray = function(){
+
+        if(this.length == 0){
+            return [];
         }
-        return false;
-    }
+        var res = [];
+        for(var i =0;i<this.length;i++){
+            if(typeof this[i] != "object"){
+                res.push(this[i]);
+            }else{
+                res.push(this[i].clone());
+            }
+        }
 
-    //数组的复制
-    Array.prototype.clone = function(){
-        return this.concat();
+        return res;
     }
 
 
@@ -150,28 +188,28 @@
 
     //2个字符串时间的比较
     // d1 < d2 --->true  else false
-    Date.prototype.compare = function(d1,d2){
+    Date.prototype.compare = function(d1){
         var tmp1 = null;
-        var tmp2 = null;
         if(typeof d1 == "string"){
             tmp1 = new Date(d1.replace(/-/g,"/"));
         }else{
             tmp1 = d1;
         }
 
-        if(typeof d1 == "string"){
-            tmp2 = new Date(d2.replace(/-/g,"/"));
-        }else{
-            tmp2 = d2;
-        }
-
-        if(tmp1 < tmp2){
+        if(Date.parse(this) > Date.parse(tmp1)){
             return true;
         }
         return false;
     }
 
-    /*****************常用的js方法******************/
+    Date.prototype.format = function(){
+        return this.getFullYear() + "-" + (this.getMonth() + 1) + "-" + this.getDate();
+    }
+
+
+    ///**********************************
+    /// 常用的js方法
+    ///**********************************
     var jsCoreMethod = {
         //获取当前元素的绝对left
         getAbsoluteLeft:function(el){
@@ -202,8 +240,13 @@
             var r = window.location.search.substr(1).match(reg);
             if(r!=null)return  decodeURIComponent(r[2]); return null;
         },
-        //一个数组里面按照时间字段来排序的方法
-        sortByDate:function(data,field){
+        /*
+        * 一个数组里面按照时间字段来排序的方法
+        * @data  数据源
+        * @field 排序日期字段
+        * @desc  升序为true 降序为true
+        * */
+        arraySortByDate:function(data,field,desc){
             if(data == undefined || data.length == 0 ){
                 return null;
             }
@@ -214,38 +257,118 @@
                     data[i]._tmpSortField = new Date(data[i][field].replace(/-/g,'/'));
                     arr.push(data[i]);
                 }
-                arr.sort(function(a,b){
-                    return a._tmpSortField - b._tmpSortField;
-                })
+                if(desc){
+                    arr.sort(function(a,b){
+                        return a._tmpSortField - b._tmpSortField;
+                    })
+                }else{
+                    arr.sort(function(a,b){
+                        return b._tmpSortField - a._tmpSortField;
+                    })
+                }
+
                 return arr;
             }
-            else if(data[0][field] instanceof Date){
-                data.sort(function(a,b){
+            else{
+                var res = data.cloneArray();
+                if(desc){
+                    res.sort(function(a,b){
+                        return a[field] - b[field];
+                    })
+                }else{
+                    res.sort(function(a,b){
+                        return b[field] - a[field];
+                    })
+                }
+
+                return res;
+            }
+        },
+        arraySortByNumber:function(data,field,desc){
+            if(data == undefined || data.length == 0 ){
+                return null;
+            }
+            var res = data.cloneArray();
+            if(desc){
+                res.sort(function(a,b){
                     return a[field] - b[field];
                 })
+            }else{
+                res.sort(function(a,b){
+                    return b[field] - a[field];
+                })
+            }
+
+            return res;
+        },
+        arraySortByChar:function(data,field,desc){
+
+            if(data == undefined || data.length == 0 ){
+                return null;
+            }
+            var res = data.cloneArray();
+            if(desc){
+                res.sort(function(a,b){
+                    return a[field].localeCompare(b[field]);
+                })
+            }else{
+                res.sort(function(a,b){
+                    return b[field].localeCompare(a[field]);
+                })
+            }
+
+            return res;
+        },
+        arraySortByField:function(data,field,desc){
+            if(data == undefined || data.length == 0 ){
+                return null;
+            }
+
+            var res = data.cloneArray();
+
+            var tmp = res[0][field];
+            //除去日期对象,其他对象类型不排序
+            if(typeof tmp == "object" && tmp instanceof Date != true){
                 return data;
             }
-            else{
-                return data;
+
+            //日期判断
+            if(typeof tmp == "object" && tmp instanceof Date){
+                res = this.arraySortByDate(res,field,desc);
+            }else{
+                //数字判断
+                if(typeof tmp == "number"){
+                    res = this.arraySortByNumber(res,field,desc);
+                }
+                if(typeof tmp == "string"){
+                    //date判断
+                    if(!/invalid/i.test(new Date(tmp))){
+                        res = this.arraySortByDate(res,field,desc);
+                    }
+                    //字符串判断
+                    else{
+                        res = this.arraySortByChar(res,field,desc);
+                    }
+                }
             }
+
+            return res;
         },
         //获取 num1 到 num2 的随机整数
         getRandom:function(num1,num2){
             var choices = num2 - num1 + 1;
             return Math.floor(Math.random() * choices + num1);
         },
-        string2Json:function(str){
-            if(str){
-                return JSON.parse(str);
+        convertStringJson:function(val){
+            if(!val){
+                return null;
             }
-            return {};
-        },
-        json2String:function(json){
-            if(json){
-                return JSON.stringify(json);
+            if(typeof val != "object"){
+                return JSON.parse(val);
             }
-            return "";
+            else{
+                return JSON.stringify(val);
+            }
         }
-
     }
 
