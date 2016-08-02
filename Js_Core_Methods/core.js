8686b95dcdd1bb16a3996240e2437c1cdfbb55a3
Stringbao <String_bao@163.com>
2016-08-02 17:22:39 +0800

    edit

--- Js_Core_Methods/core.js
+++ Js_Core_Methods/core.js
@@ -653,10 +653,17 @@
             }
             return {bl:true,msg:""};
         },
-        validateIsNull:function(str,msg){
+        validateIsNull:function(str,msg,len){
+            if(!len){
+                len = 999999;
+            }
             if(str && str.trim().length >0){
                 return {bl:true,msg:""};
             }
+            if(str.length > len){
+                return {bl:false,msg:""};
+            }
+
             return {bl:false,msg:msg};
         },
         validateEmail:function(str,msg){
@@ -672,5 +679,24 @@
                 return {bl:true,msg:""};
             }
             return {bl:false,msg:msg};
+        },
+        validataUrl:function(str,msg){
+            var strRegex = "^((https|http|ftp|rtsp|mms)?://)"
+                + "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" //ftp的user@
+                + "(([0-9]{1,3}\.){3}[0-9]{1,3}" // IP形式的URL- 199.194.52.184
+                + "|" // 允许IP和DOMAIN（域名）
+                + "([0-9a-z_!~*'()-]+\.)*" // 域名- www.
+                + "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\." // 二级域名
+                + "[a-z]{2,6})" // first level domain- .com or .museum
+                + "(:[0-9]{1,4})?" // 端口- :80
+                + "((/?)|" // a slash isn't required if there is no file name
+                + "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";
+            var re=new RegExp(strRegex);
+
+            if (re.test(str)){
+                return {bl:true,msg:msg};
+            }else{
+                return {bl:false,msg:msg};
+            }
         }
     }
