1b5fbae0ade278399a26d67465452b58ce91ff94
Stringbao <String_bao@163.com>
2016-08-28 15:50:14 +0800

    edit

--- Js_Core_Methods/core.js
+++ Js_Core_Methods/core.js
@@ -738,5 +738,46 @@
                     })
                 }
             });
+        },
+        compress: function(source_img_obj, quality, output_format){
+
+            var mime_type = "image/jpeg";
+            if(output_format!=undefined && output_format=="png"){
+                mime_type = "image/png";
+            }
+            var cvs = document.createElement('canvas');
+            //naturalWidth真实图片的宽度
+            cvs.width = source_img_obj.naturalWidth;
+            cvs.height = source_img_obj.naturalHeight;
+            var ctx = cvs.getContext("2d").drawImage(source_img_obj, 0, 0);
+            var newImageData = cvs.toDataURL(mime_type, quality/100);
+            var result_image_obj = new Image();
+            result_image_obj.src = newImageData;
+            return result_image_obj;
+        },
+        uploadCompressImg:function(id,cb,url){
+            $("body").on("change","#"+id,function(e){
+                var val = e.target.files || e.dataTransfer.files;
+
+                for(var i=0;i<val.length;i++){
+                    (function(file){
+                        var filereader = new FileReader();
+                        var suffix = file.name.substring(file.name.indexOf('.')+1);
+                        filereader.onload = function () {
+
+                            var tempImg = $("<img />");
+                            tempImg.attr("src",this.result).hide();
+                            $("body").append(tempImg);
+
+                            var compress = jsCoreMethod.compress(tempImg.get(0),50);
+                            jsCoreMethod.jqPostData(url,compress.src,function(d){
+                                cb(d);
+                                $(tempImg).remove();
+                            })
+                        }
+                        filereader.readAsDataURL(file);
+                    })(val[i]);
+                }
+            })
         }
     }
